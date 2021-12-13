53161d90fa27c7372088999a418b2f92cbbf1f3b
ImSupporter <adam990901@gmail.com>
2021-12-13 22:12:49 +0900

    Post Detail layout 추
    -post detail 레이아웃 추가
    -item_image scaleType : centercrop 추가

--- app/src/main/java/com/example/qans/PostAdapter.kt
+++ app/src/main/java/com/example/qans/PostAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class PostAdapter(private val postList: ArrayList<Post>) : RecyclerView.Adapter<PostAdapter.CustomViewHolder> (){
